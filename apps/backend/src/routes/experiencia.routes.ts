3df00676aac539acaf6316500525f1b1e3f1e246
Aldana Vallejos <aldanavs@outlook.com>
2026-05-10 16:32:31 -0300

    modify req

--- apps/backend/src/routes/experiencia.routes.ts
+++ apps/backend/src/routes/experiencia.routes.ts
@@ -20,7 +20,7 @@ router.post("/", async (req: Request, res: Response) => {
 router.get("/:id", async (req: Request, res: Response) => {
   try {
     const useCase = new ObtenerExperiencia(experienciaRepo, preguntaRepo)
-    const resultado = await useCase.execute(req.params.id)
+    const resultado = await useCase.execute(req.params.id as string)
     res.status(200).json(resultado)
   } catch (error: any) {
     res.status(404).json({ error: error.message })
